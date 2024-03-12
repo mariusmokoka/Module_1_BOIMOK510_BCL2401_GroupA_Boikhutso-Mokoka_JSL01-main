868c9e5d3150831d3691e8f7c9e8868aedc6ac42
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-03-12 14:17:44 +0200

    update

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     let result = ''; // Placeholder for validation result
 
-    // Regular expression to match 'pet_' followed by alphanumeric characters
+    // Regular expression to check if input starts with 'pet_' and is followed by alphanumeric characters
     let regex = /^pet_[a-zA-Z0-9]+$/;
 
     if (regex.test(input)) {
-        result = 'Valid syntax 🟢';
+        result = 'Valid Syntax 🟢';
     } else {
-        result = 'Invalid syntax 🔴';
+        result = 'Invalid Syntax 🔴';
     }
 
     document.getElementById('result').innerText = result;
